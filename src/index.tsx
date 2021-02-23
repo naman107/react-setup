834f8b12277b0c70e2ed114cf56cac7f0329f09f
Naman.Agarwal <naman.agarwal@ril.com>
2021-02-23 16:28:54 +0530

    React TS setup

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App(): JSX.Element {
+    return (
+        <h1>
+            Hello
+        </h1>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app-root'));
